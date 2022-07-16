b78b6360ae7ddec1f4d9007ed1584a7aab4975fb
leehj050211 <leehj050211@gmail.com>
2022-07-16 16:57:05 +0900

    initial commit

---
+++ src/main/java/bssm/bsm/BsmApplication.java
@@ -0,0 +1,13 @@
+package bssm.bsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bssm/bsm/BsmApplicationTests.java
@@ -0,0 +1,13 @@
+package bssm.bsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
