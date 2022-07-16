503a7e4c0bda8a32003c9410e2e60f86f8906a2f
leehj050211 <leehj050211@gmail.com>
2022-07-16 21:44:27 +0900

    Add: 환경 변수 설정, DB 연결

---
+++ src/main/java/bssm/bsm/env/PropertyConfig.java
@@ -0,0 +1,11 @@
+package bssm.bsm.env;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:env.properties")
+})
+public class PropertyConfig {}
