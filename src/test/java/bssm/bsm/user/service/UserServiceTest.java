d35b720f8deb531dbbd5fbe37feda015340d2b57
leehj050211 <leehj050211@gmail.com>
2022-07-25 21:33:32 +0900

    Add: 유저 비밀번호 및 닉네임 변경 테스트, Fix: 유저 테스트 버그

--- src/test/java/bssm/bsm/user/service/UserServiceTest.java
+++ src/test/java/bssm/bsm/user/service/UserServiceTest.java
@@ -3,11 +3,12 @@ package bssm.bsm.user.service;
 import bssm.bsm.user.UserService;
 import bssm.bsm.user.dto.request.UserLoginDto;
 import bssm.bsm.user.dto.request.UserSignUpDto;
+import bssm.bsm.user.dto.request.UserUpdateNicknameDto;
+import bssm.bsm.user.dto.request.UserUpdatePwDto;
 import bssm.bsm.user.entities.Student;
 import bssm.bsm.user.entities.User;
 import bssm.bsm.user.repositories.StudentRepository;
 import bssm.bsm.user.repositories.UserRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,11 +30,11 @@ class UserServiceTest {
     @DisplayName("유저 회원 가입 테스트")
     void signUp() throws Exception {
         // Initialization
-        studentRepository.findByAuthCode("A1B2C3").ifPresent(
-                student -> {studentRepository.delete(student);}
-        );
         userRepository.findById("testId").ifPresent(
                 user -> {userRepository.delete(user);}
+        );
+        studentRepository.findByAuthCode("A1B2C3").ifPresent(
+                student -> {studentRepository.delete(student);}
         );
         Student studentInfo = new Student("12345", true, "A1B2C3", 1, 2021, 1, 2, 3, "testName", "test@test.com");
         studentRepository.save(studentInfo);
@@ -69,6 +70,44 @@ class UserServiceTest {
         // Check
         if (!user.getNickname().equals(testUser.getNickname())) {
             throw new IllegalStateException("유저 로그인 테스트 실패");
+        }
+    }
+
+    @Test
+    @DisplayName("유저 비밀번호 변경 테스트")
+    void updatePw() throws Exception {
+        // Initialization
+        this.signUp();
+        User user = userRepository.findById("testId").get();
+
+        // Test
+        UserUpdatePwDto invalidDto = new UserUpdatePwDto("newPw", "newPw1");
+        try {
+            userService.updatePw(user, invalidDto);
+            throw new IllegalStateException("비밀번호 재입력 테스트 실패");
+        } catch (Exception ignored) {}
+        UserUpdatePwDto validDto = new UserUpdatePwDto("newPw", "newPw");
+        userService.updatePw(user, validDto);
+
+        // Check
+        UserLoginDto dto = new UserLoginDto("testId", "newPw");
+        userService.login(dto);
+    }
+
+    @Test
+    @DisplayName("유저 닉네임 변경 테스트")
+    void updateNickname() throws Exception {
+        // Initialization
+        this.signUp();
+        User user = userRepository.findById("testId").get();
+
+        // Test
+        UserUpdateNicknameDto dto = new UserUpdateNicknameDto("newNickname");
+        User newUser = userService.updateNickname(user, dto);
+
+        // Check
+        if (!newUser.getNickname().equals("newNickname")) {
+            throw new IllegalStateException("유저 닉네임 변경 테스트 실패");
         }
     }
 }
